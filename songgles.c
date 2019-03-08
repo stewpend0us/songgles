ad9d32be295f760e189e6c1bf58fa17b1f68cdf0
stewart <stewart.wadsworth@gmail.com>
2019-03-07 22:43:37 -0500

    emit might be working?

--- songgles.c
+++ songgles.c
@@ -1,12 +1,8 @@
 #include "songgles.h"
 
-char state;
 
-enum state { WAIT, CHIRP, LISTEN, BEEP, };
-
-static inline void setup_io()
+static void setup_io()
 {
-	
 	PC_ODR = 0x00; // output off
 	PC_DDR = 0x48; // output on 3 and 6
 	PC_CR1 = 0x48; // push pull on 3 and 6
@@ -18,31 +14,26 @@ static inline void setup_io()
 	EXTI_CR1 |= 0x80; // falling edge for port D
 }
 
-static inline void setup_clock()
+static void setup_clock()
 {
 	CLK_SWCR |= 0x02; //set SWEN
 	CLK_SWCR &= ~0x04; //0 SWIEN
 	CLK_SWR = 0xB4; //HSE clock source
 //	CLK_CKDIVR = 0x18; // probably don't need to do this
 //	while ( !(CLK_SWCR & 0x08) ) {} // probably don't need to do this
-	CLK_ICKR &= ~0x01; // disable high-speed internal RC
+	CLK_ICKR &= ~0x01; // disable high-speed internal clock
 }
 
 static void setup_TIM1()
 {
 	TIM1_PSCRH = 0;
 	TIM1_PSCRL = 2;
+//	TIM1_PSCRH = 0x8F;
+//	TIM1_PSCRL = 0xFF;
 	TIM1_ARRH = 0;
 	TIM1_ARRL = 100;
 	TIM1_CR1 |= 0x80; // use ARR preload registers
 //	TIM1_IER |= 0x01; // enable interrupt
-}
-
-void tim1_update(void) __interrupt(11)
-{
-	PC_ODR ^= 0x40;
-	PC_ODR ^= 0x08;
-	TIM1_SR1 &= ~0x01;
 }
 
 void tim4_update(void) __interrupt(23)
@@ -51,33 +42,70 @@ void tim4_update(void) __interrupt(23)
 	TIM2_IER &= ~0x01; // disable TIM2 interrupt
 
 	PC_ODR |= 0x08; // start the chirp
-	TIM1_IER |= 0x01; // enable TIM1 interrupt
+	TIM1_CR1 |= 0x01; // enable TIM1 counter
 }
+
+enum { CHIRP_START, CHIRPING, LISTEN_START, LISTENING, WAITING };
+unsigned char chirps_remaining;
+unsigned char state;
 
 int main(void)
 {
-	sim();
-	setup_clock();
-	setup_io();
-	setup_TIM1();
-	rim();
+	__critical
+	{
+		setup_clock();
+		setup_io();
+		setup_TIM1();
+	}
 
-	TIM1_CR1 |= 0x01;
 
-	state = WAIT;
 	for(;;)
 	{
-		switch state
+		switch ( state )
 		{
-			case WAIT:
+			case CHIRP_START:
+			{
+				state = CHIRPING; // update state
+				chirps_remaining = 16;
+				PC_ODR |= 0x08; // start the chirp
+				TIM1_CR1 |= 0x01; // enable TIM1 counter
+				break;
+			}
+			case CHIRPING:
+			{
+				if ( TIM1_SR1 & 0x01 ) // TIM1 overflow
+				{
+					TIM1_SR1 &= ~0x01;
+					if ( --chirps_remaining )
+					{
+					//	PD_ODR ^= 0x40;
+						PC_ODR ^= 0x48;
+					}
+					else
+					{
+						TIM1_CR1 &= ~0x01; // disable TIM1 counter
+					//	PD_ODR = 0x00;
+						PC_ODR = 0x00;
+						state = LISTEN_START;
+					}
+				}
+				break;
+			}
+			case LISTEN_START:
+			{
+				state = LISTENING;
+				break;
+			}
+			case LISTENING:
+			{
+				state = WAITING;
+				break;
+			}
+			case WAITING:
+			{
 				wfi();
 				break;
-			case CHIRP:
-				break;
-			case LISTEN:
-				break;
-			case BEEP:
-				break;
+			}
 		}
 	}
 }
